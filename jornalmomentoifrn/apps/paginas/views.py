c0b6d8c3ee12920bb60dbc6ef7c06207d0e4c44c
Arthur Lola Oliveira <arthurlolaoliveira@hotmail.com>
2025-01-13 11:51:59 -0300

    add mentes html

--- jornalmomentoifrn/apps/paginas/views.py
+++ jornalmomentoifrn/apps/paginas/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 # Create your views here.
 
+def mentes(request):
+  return render(request, 'paginas/mentes.html')
+
 class IndexViews(TemplateView):
   template_name = "paginas/index.html"
 
 class ModeloViews(TemplateView):
   template_name = "paginas/modelo.html"
+
