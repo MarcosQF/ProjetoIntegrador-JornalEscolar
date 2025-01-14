ab3fba720708da611b220712288c88af84cfb61d
Marcos Queiroz <marcosamfq123@gmail.com>
2025-01-14 00:00:41 -0300

    criando tela, listar noticias

--- jornalmomentoifrn/apps/adminhub/urls.py
+++ jornalmomentoifrn/apps/adminhub/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path,include
-from .views import InitialDashboardViews,UsersDashboardViews,CreatePostViews
+from .views import InitialDashboardViews,UsersDashboardViews,CreatePostViews,PostsDashboardViews
 
 urlpatterns = [
     path('', InitialDashboardViews.as_view(), name="initial-dashboard-path"),
     path('usuarios/', UsersDashboardViews.as_view(), name="users-dashboard-path"),
-    path('criar_noticia/', CreatePostViews.as_view(), name="create-post-path"),  
+    path('noticias/criar_noticia/', CreatePostViews.as_view(), name="create-post-path"),
+    path('noticias/', PostsDashboardViews.as_view(), name="posts-path"),
 ]
