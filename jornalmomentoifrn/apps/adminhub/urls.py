92f28a07632d5767c64c36abf49a2e44d5310c95
Marcos Queiroz <marcosamfq123@gmail.com>
2025-01-14 01:59:20 -0300

    criando comentários denunciados

--- jornalmomentoifrn/apps/adminhub/urls.py
+++ jornalmomentoifrn/apps/adminhub/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path,include
-from .views import InitialDashboardViews,UsersDashboardViews,CreatePostViews,PostsDashboardViews
+from .views import InitialDashboardViews,ReportedCommentsViews,UsersDashboardViews,CreatePostViews,PostsDashboardViews
 
 urlpatterns = [
     path('', InitialDashboardViews.as_view(), name="initial-dashboard-path"),
     path('usuarios/', UsersDashboardViews.as_view(), name="users-dashboard-path"),
     path('noticias/criar_noticia/', CreatePostViews.as_view(), name="create-post-path"),
     path('noticias/', PostsDashboardViews.as_view(), name="posts-path"),
+    path('comentarios_denunciados/', ReportedCommentsViews.as_view(), name="comments-path"),
+    
 ]
