52058e6ae0069d96c404eafaec73b6700f839b14
Luis Felipe <dantas.luis@escolar.ifrn.edu.br>
2025-01-22 12:40:04 -0300

    alteração derotas e criação depaginas

--- jornalmomentoifrn/apps/paginas/urls.py
+++ jornalmomentoifrn/apps/paginas/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import IndexViews, ModeloViews,MentesViews
+from .views import IndexViews, ModeloViews, MentesViews, NoticiasViews, LogadoViews, PedidosViews, BaseViews, PerfilViews
 
 urlpatterns = [
     path('', IndexViews.as_view(), name="index"),
+    path('base/', BaseViews.as_view(), name="base"),
     path('modelo/', ModeloViews.as_view(), name="modelo"),
     path('mentes/', MentesViews.as_view(), name="mentes"),
+    path('noticias/', NoticiasViews.as_view(), name="noticias"),
+    path('logado/', LogadoViews.as_view(), name="logado"),
+    path('pedidos/', PedidosViews.as_view(), name="pedidos"),
+    path('perfil/', PerfilViews.as_view(), name="perfil"),
+    
 ]
 
--- jornalmomentoifrn/apps/paginas/views.py
+++ jornalmomentoifrn/apps/paginas/views.py
@@ -3,10 +3,25 @@ from django.views.generic import TemplateView
 # Create your views here.
 
 class IndexViews(TemplateView):
-  template_name = "paginas/index.html"
+  template_name = "paginas/modelo.html"
 
 class ModeloViews(TemplateView):
   template_name = "paginas/modelo.html"
 
 class MentesViews(TemplateView):
   template_name = "paginas/mentes.html"
+  
+class NoticiasViews(TemplateView):
+  template_name = "paginas/noticias.html"
+
+class LogadoViews(TemplateView):
+  template_name = "paginas/logado.html"
+  
+class PedidosViews(TemplateView):
+  template_name = "paginas/pedidos.html"
+  
+class BaseViews(TemplateView):
+  template_name = "paginas/base_paginas.html"
+  
+class PerfilViews(TemplateView):
+  template_name = "paginas/perfil.html"
